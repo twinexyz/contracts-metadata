a0431431a0200afde33abc02ed127f985faba4d5
lilixac <hinewton25@gmail.com>
2025-01-22 08:02:50 +0545

    chore: update events

--- src/evm/ethereum/l1_message_queue.rs
+++ src/evm/ethereum/l1_message_queue.rs
@@ -7,23 +7,23 @@ sol! {
         #[derive(Debug)]
         event QueueDepositTransaction(
             uint64 nonce,
-            string to_twine_address,
-            string l1_token,
-            string l2_token,
             uint64 chainId,
-            string amount,
-            uint64 block_number
+            uint64 block_number,
+            uint256 amount,
+            address to_twine_address,
+            address l1_token,
+            address l2_token
         );
 
         #[derive(Debug)]
         event QueueWithdrawalTransaction(
             uint64 nonce,
-            string to_twine_address,
-            string l1_token,
-            string l2_token,
             uint64 chainId,
-            string amount,
-            uint64 block_number
+            uint64 block_number,
+            uint256 amount,
+            address to_twine_address,
+            address l1_token,
+            address l2_token
         );
     }
 }
