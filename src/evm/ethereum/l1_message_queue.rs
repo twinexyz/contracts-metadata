e82b634a6449db9e903231ce9a92cfa80ad8f3fe
Newton <57525074+lilixac@users.noreply.github.com>
2025-01-23 10:17:09 +0545

    Update l1_message_queue.rs

--- src/evm/ethereum/l1_message_queue.rs
+++ src/evm/ethereum/l1_message_queue.rs
@@ -5,6 +5,18 @@ sol! {
     contract L1MessageQueue{
 
         #[derive(Debug)]
+        struct MessageData {
+            uint64 nonce;
+            uint64 chainId;
+            uint64 blockNumber;
+            string fromAddress;
+            string toAddress;
+            string l1Token;
+            string l2Token;
+            string amount;
+        }
+
+        #[derive(Debug)]
         event QueueDepositTransaction(
             uint64 nonce,
             uint64 chainId,
