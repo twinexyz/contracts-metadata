894f2c38c819f1a2240a759e8dba67150a7b0016
Newton <57525074+lilixac@users.noreply.github.com>
2025-02-20 08:01:04 +0545

    chore: update message queue (#3)

--- src/evm/ethereum/l1_message_queue.rs
+++ src/evm/ethereum/l1_message_queue.rs
@@ -20,24 +20,24 @@ sol! {
         event QueueDepositTransaction(
             uint64 nonce,
             uint64 chainId,
-            uint64 block_number,
-            address l1_token,
-            address l2_token,
+            uint64 blockNumber,
+            address l1Token,
+            address l2Token,
             address from,
-            address to_twine_address,
-            uint256 amount,
+            address toTwineAddress,
+            uint256 amount
         );
 
         #[derive(Debug)]
         event QueueWithdrawalTransaction(
             uint64 nonce,
             uint64 chainId,
-            uint64 block_number,
-            address l1_token,
-            address l2_token,
+            uint64 blockNumber,
+            address l1Token,
+            address l2Token,
             address from,
-            address to_twine_address,
-            uint256 amount,
+            address toTwineAddress,
+            uint256 amount
         );
     }
 }
